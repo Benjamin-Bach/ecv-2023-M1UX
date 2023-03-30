a4b3fb2bd11fd5f64ca6cc0cd6db5fb270db82cc
Benjamin-Bach <mail@benjaminbach.fr>
2023-03-30 11:27:28 +0200

    Add files via upload
    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+
+
+document.querySelector('.toggle-menu').addEventListener('click', () => {
+    document.querySelector('.main-menu').classList.toggle('close')
+    // Vous pourriez aussi utiliser cette ligne dans l'attribut "onclick"
+    // de l'élément div.main-menu 
+})
+
